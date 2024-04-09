c0a07246cfacbf7af678cfa97ff1887c92680403
Félix MARQUET <fpmovie4@gmail.com>
2024-04-09 18:54:47 +0200

    Update with the new path

--- startup.sh
+++ startup.sh
@@ -6,25 +6,26 @@
 
 # Démarrer le serveur TCP
 echo "Starting the TCP server"
-/home/modelec/serge/tcp-server &
+/home/modelec/Serge/TCPSocketServer/build/socketServer &
 sleep 1
 
 # Démarrer le Lidar
 echo "Starting the Lidar"
-/home/modelec/serge/lidar &
+/home/modelec/Serge/detection_adversaire/build/lidar &
 sleep 1
 
 # Démarrer la caméra
 echo "Starting the camera"
-/home/modelec/serge/camera &
+/home/modelec/Serge/detection_pot/build/arucoDetector /home/modelec/Serge/detection_pot/build/camera_calibration.yml 8080 --headless &
+sleep 1
 
 # Démarrer l'IHM
 echo "Starting the IHM"
-/home/modelec/serge/ihm fullscreen &
+/home/modelec/Serge/ihm/build/ihm_robot fullscreen &
 
 # Fonction pour surveiller la fermeture de l'IHM
 monitor_all() {
-    local pids=$(pgrep -d ' ' -f "tcp-server|lidar|camera|ihm")
+    local pids=$(pgrep -d ' ' -f "socketServer|lidar|arucoDetector|ihm_robot")
     
     # Attendre que l'un des programmes se termine
     wait $pids
@@ -33,7 +34,7 @@ monitor_all() {
     for pid in $pids; do
         if ps -p $pid > /dev/null; then
             echo "Program with PID $pid has terminated, stopping other programs"
-            pkill -P $$ -f "tcp-server|lidar|camera|ihm"
+            pkill -P $$ -f "socketServer|lidar|arucoDetector|ihm_robot"
             break
         fi
     done
@@ -48,4 +49,4 @@ wait
 
 # Retourner à la position de départ
 echo "Put SERGE back in the starting position"
-/home/modelec/serge/starting-position
+/home/modelec/Serge/starting-position
