37a63f3d59e125932745b9123d3fd740fb82dfba
breizhhardware <fpmovie4@gmail.com>
2024-04-18 20:51:27 +0200

    Add logging

--- startup.sh
+++ startup.sh
@@ -17,6 +17,7 @@ fi
 # Démarrer le serveur TCP
 echo "Starting the TCP server"
 /home/modelec/Serge/TCPSocketServer/build/socketServer $port &
+/home/modelec/Serge/TCPSocketServer/build/socketServer & > /home/modelec/Serge/Logs/socketServer.log
 echo "TCP server pid" $! > /home/modelec/Serge/TCP_pid.txt
 pidserver+=($!)
 sleep 1
@@ -24,6 +25,7 @@ sleep 1
 # Démarrer le Lidar
 echo "Starting the Lidar"
 screen -dmS lidar /home/modelec/Serge/detection_adversaire/build/lidar $port
+screen -dmS lidar /home/modelec/Serge/detection_adversaire/build/lidar > /home/modelec/Serge/Logs/lidar.log
 pidLidar=$(screen -ls | grep -o '[0-9]*\.lidar' | grep -o '[0-9]*')
 echo "Lidar pid" $pidLidar > /home/modelec/Serge/Lidar_pid.txt
 pids+=($pidLidar)
@@ -32,6 +34,7 @@ sleep 1
 # Démarrer l'IHM
 echo "Starting the IHM"
 /home/modelec/Serge/ihm/build/ihm_robot fullscreen $port &
+/home/modelec/Serge/ihm/build/ihm_robot fullscreen & > /home/modelec/Serge/Logs/ihm_robot.log
 echo "IHM pid" $! > /home/modelec/Serge/IHM_pid.txt
 pids+=($!)
 sleep 1
@@ -39,6 +42,7 @@ sleep 1
 # Démarrer la caméra
 echo "Starting the camera"
 screen -dmS camera /home/modelec/Serge/detection_pot/build/arucoDetector /home/modelec/Serge/detection_pot/build/camera_calibration.yml $port --headless
+screen -dmS camera /home/modelec/Serge/detection_pot/build/arucoDetector /home/modelec/Serge/detection_pot/build/camera_calibration.yml 8080 --headless > /home/modelec/Serge/Logs/camera.log
 pidCam=$(screen -ls | grep -o '[0-9]*\.camera' | grep -o '[0-9]*')
 echo "Camera pid" $pidCam > /home/modelec/Serge/Camera_pid.txt
 pids+=($pidCam)
@@ -47,6 +51,7 @@ sleep 1
 # Démarrer le programme d'interconnexion raspi -> arduino
 echo "Starting the interconnection program"
 screen -dmS connectors /home/modelec/Serge/connectors/build/connectors $port
+screen -dmS connectors /home/modelec/Serge/connectors/build/connectors > /home/modelec/Serge/Logs/connecors.log
 pid=$(screen -ls | grep -o '[0-9]*\.connectors' | grep -o '[0-9]*')
 echo "Interconnection pid" $pid > /home/modelec/Serge/Interconnection_pid.txt
 pids+=($pid)
@@ -55,6 +60,7 @@ sleep 1
 # Démarrer le programme de contrôle des servomoteurs
 echo "Starting the servomotor control program"
 screen -dmS servo_motor /home/modelec/Serge/servo_moteurs/build/servo_motor $port
+screen -dmS servo_motor /home/modelec/Serge/servo_moteurs/build/servo_motor > /home/modelec/Serge/Logs/servo_motor.log
 pid=$(screen -ls | grep -o '[0-9]*\.servo_motor' | grep -o '[0-9]*')
 echo "Servomotor pid" $pid > /home/modelec/Serge/Servomotor_pid.txt
 pids+=($pid)
@@ -63,6 +69,7 @@ sleep 1
 # Démarrer le programme de la tirette
 echo "Starting the tirette program"
 screen -dmS tirette /home/modelec/Serge/tirette/tirette $port
+screen -dmS tirette /home/modelec/Serge/tirette/tirette > /home/modelec/Serge/Logs/tirette.log
 pid=$(screen -ls | grep -o '[0-9]*\.tirette' | grep -o '[0-9]*')
 echo "Tirette pid" $pid > /home/modelec/Serge/Tirette_pid.txt
 pids+=($pid)
@@ -109,3 +116,8 @@ sleep 1
 # Retourner à la position de départ
 echo "Put SERGE back in the starting position"
 /home/modelec/Serge/emergency/build/emergency ./end_point.txt &
+/home/modelec/Serge/starting-position > /home/modelec/Serge/Logs/starting-position.log
+
+sleep 2
+
+screen -wipe
